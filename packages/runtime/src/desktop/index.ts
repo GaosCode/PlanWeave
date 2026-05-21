86e0dcd45165870ffcaa6690034a1a0742d68ba1
MrBrain <176294248+GaosCode@users.noreply.github.com>
2026-05-21 21:13:53 +0800

    feat(runtime): export desktop runtime APIs

---
+++ packages/runtime/src/desktop/index.ts
@@ -0,0 +1,57 @@
+export {
+  getProjectOverview,
+  initOrOpenProject,
+  listProjects,
+  openProject
+} from "./projectApi.js";
+export {
+  addContextNode,
+  addBlock,
+  addDependencyEdge,
+  addTaskNode,
+  createTaskDraft,
+  getBlockDetail,
+  getGraphViewModel,
+  getStatistics,
+  getTaskDetail,
+  getTaskExecutionOrder,
+  getTodoGroups,
+  removeBlock,
+  removeDependencyEdge,
+  removeTaskNode,
+  searchProject,
+  updateBlockExecutor,
+  updateBlockPrompt,
+  updateBlockTitle,
+  updateTaskExecutor,
+  updateTaskPrompt,
+  updateTaskTitle,
+  validateGraphEdit
+} from "./graphApi.js";
+export { getDesktopLayout, resetDesktopLayout, saveDesktopLayout } from "./layoutApi.js";
+export {
+  createDesktopPackageFileSnapshot,
+  detectDesktopPackageFileChanges,
+  getDirtyPromptRefs,
+  refreshChangedDesktopPackagePrompts,
+  refreshPackageFileChanges
+} from "./fileSyncApi.js";
+export {
+  getAutoRunState,
+  getLatestAutoRunSummary,
+  pauseAutoRun,
+  resumeAutoRun,
+  startAutoRun,
+  stopAutoRun
+} from "./runApi.js";
+export {
+  getFeedbackRecords,
+  getReviewAttempts,
+  getRunRecord,
+  listBlockRunRecords
+} from "./recordsApi.js";
+export {
+  getReviewPipeline,
+  updateReviewPipeline
+} from "./reviewPipelineApi.js";
+export type * from "./types.js";
